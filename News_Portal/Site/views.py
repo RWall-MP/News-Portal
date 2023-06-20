5a205b2f0573bf22c2240c4cd6fa883083bcdc32
RWall-MP <my.post.005.rewan@gmail.com>
2023-06-20 22:02:15 +0500

    T

---
+++ News_Portal/Site/views.py
@@ -0,0 +1,9 @@
+from django.views.generic import ListView
+from .models import Post
+
+
+class PostsList(ListView):
+    model = Post
+    ordering = 'title'
+    template_name = 'posts.html'
+    context_object_name = 'posts'
