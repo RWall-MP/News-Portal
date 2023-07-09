fc3aeb9fa7991ce1b4ebc74395eeba63b1477f13
RWall-MP <my.post.005.rewan@gmail.com>
2023-07-09 23:53:14 +0500

    Настроена отправка писем

--- News_Portal/News_Portal/settings.py
+++ News_Portal/News_Portal/settings.py
@@ -153,12 +153,12 @@ ACCOUNT_EMAIL_VERIFICATION = 'none'
 ACCOUNT_FORMS = {"signup": "accounts.forms.CustomSignupForm"}
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_HOST_USER = 'my.post.005.rewan@gmail.com'
-EMAIL_HOST_PASSWORD = '89090112411AaBb'
-EMAIL_USE_TLS = True
-EMAIL_USE_SSL = False
-DEFAULT_FROM_EMAIL = 'my.post.005.rewan@gmail.com'
+EMAIL_HOST = 'smtp.yandex.ru'
+EMAIL_PORT = 465
+EMAIL_HOST_USER = 'r.wall.post'
+EMAIL_HOST_PASSWORD = 'omclbpojfrtsgbyf'
+EMAIL_USE_TLS = False
+EMAIL_USE_SSL = True
+DEFAULT_FROM_EMAIL = 'r.wall.post@yandex.ru'
 
 SITE_ID = 1
